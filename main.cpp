911c2a328f87fe7bb625d0f0e64677e016039e0e
Andrew_Yankee <144984404+CS230350@users.noreply.github.com>
2023-09-29 12:48:10 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,78 @@
+//
+//  main.cpp
+//  lesson1
+//
+//  Created by Dmitriy Taldonov on 29/09/23.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+// const float pi = 3.14
+//    pi = 4.56
+//    cout << pi << endl;
+    
+//    int length = 10;
+//    float weight = 2.34;
+//    double num = 6.78765;
+//    char grade = 'A';
+//    string name = "Hello";
+//    bool is_better = true;
+//
+//    cout <<length << " " << weight << " " << num << " " << " " << grade << is_better << endl;
+    
+//    int a = 4, b = 5;
+//    int x, y;
+//    x = 10;
+//    y= 20;
+//    int result;
+//    result = y + b;
+//
+//    cout << result << endl;
+////    addition
+//    cout << a + b << endl;
+//    cout << x + a << endl;
+////    division
+//    cout << x / a <<endl;
+////    subtraction
+//    result = y - x;
+//
+//    cout << result <<endl;
+//    modulus
+//    result = x % a;
+//    cout << result <<endl;
+//
+//    int c, d;
+//    c = 3;
+//    d = 20;
+//    x      %=6;
+//    cout << x << endl;
+//
+    
+//    int int_num = 15;
+//    float float_num = 4.3;
+//    double double_num = 76.3982;
+//    bool bool_type = false;
+//    char char_name = '$';
+//    cout << int_num << " " << float_num << " " << double_num<< " " << bool_type<< " " <<char_name <<endl;
+//
+////   Problem_2
+//    int apple = 13;
+//    int apricot = 25;
+//    cout << apple * apricot <<endl;
+//    Problem_3
+    int x = 23;
+    int y = 87;
+    cout << "Product" << "x" << "*" << "y" << "=" << x*y << endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
